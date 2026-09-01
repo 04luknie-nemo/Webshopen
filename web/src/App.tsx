7f8f2020ae963e4c5c23cfcfbd7ae2e75ebaace8
Lukas <04luknie@skola.boras.se>
2026-09-01 14:40:37 +0200

    Installed react router and added imports to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import AdminPage from "./pages/adminPage";
 import CheckoutPage from "./pages/checkoutPage";
 import ConfirmationPage from "./pages/confirmationPage";
