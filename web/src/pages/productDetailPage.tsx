70c5712dddf105186128d30a18898b512f7152dd
Mattias <mattiasmeza94@hotmail.com>
2026-09-02 14:46:44 +0200

    Added display selected product details

--- web/src/pages/productDetailPage.tsx
+++ web/src/pages/productDetailPage.tsx
@@ -1,3 +1,15 @@
+import { useParams } from "react-router";
+import { initialProducts } from "../mockData";
+
 export default function ProductDetailPage() {
-    return(<>Product Detail Page</>)
+    const { id } = useParams();
+    const productId = Number(id);
+
+    const product = initialProducts.find((product) => product.id === productId);
+
+    if (!product) {
+        return <p>Produkten hittades inte.</p>;
+    }
+
+    return <h1>{product.title}</h1>;
 }
