fa2db5e69baacd894b4797db81dabbcd61d43d2e
Christian Olausson <olausson44@gmailcom>
2026-09-06 13:38:05 +0200

    Add order summary with total price to checkout page

--- web/src/pages/checkoutPage.tsx
+++ web/src/pages/checkoutPage.tsx
@@ -1,3 +1,28 @@
+import { Card, CardContent, Container, Typography } from "@mui/material";
+import { initialCartItems } from "../mockData";
+
 export default function CheckoutPage() {
-    return(<>Checkout Page</>)
+  let totalAmount = 0;
+  for (const item of initialCartItems) {
+    totalAmount += item.product.price * item.quantity;
+  }
+  return (
+    <Container>
+      <Typography align="center">Checkout</Typography>
+
+      <Card sx={{ maxWidth: 500, mx: "auto" }}>
+        <CardContent>
+          <ul>
+            {initialCartItems.map((item) => (
+              <li key={item.product.id}>
+                {item.product.title} Antal: {item.quantity}
+                Price: {item.product.price * item.quantity}:sek
+              </li>
+            ))}
+          </ul>
+          Totala summan:{totalAmount}
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
