d6ec0925c36991302bf055915f10642438e61f4f
Lukas <04luknie@skola.boras.se>
2026-09-07 21:11:49 +0200

    Fixed typo in config, swapped port code in .http file and added way to see database products

--- web/src/pages/adminPage.tsx
+++ web/src/pages/adminPage.tsx
@@ -1,47 +1,116 @@
-import { useState, type SubmitEvent } from "react"
-import { initialProducts } from "../mockData"
+import { useQuery } from "@tanstack/react-query";
+import { useState, type SubmitEvent } from "react";
+import { initialProducts } from "../mockData";
 import type { Product } from "../types";
+
+async function getAllProducts() {
+  const res = await fetch("/v1/products");
+  return res.json();
+}
+
 export default function AdminPage() {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [price, setPrice] = useState<number | "">("");
-    const [imageUrl, setImageUrl] = useState("");
-    const [stock, setStock] = useState<number | "">("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState<number | "">("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [stock, setStock] = useState<number | "">("");
 
-    function addProduct(event: SubmitEvent<HTMLFormElement>) {
-        event.preventDefault();
+  const query = useQuery({
+    queryKey: ["products"],
+    queryFn: getAllProducts,
+  });
 
-        if (stock === "" || price === "")
-            return;
+  function addProduct(event: SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-        const newProduct: Product = {
-            id: initialProducts.length + 1,
-            title: title,
-            description: description,
-            price: price,
-            imageUrl: imageUrl,
-            stock: stock
-        }
-        console.log(newProduct);
-        initialProducts.push(newProduct);
-        // Gör om till json sträng och sätter products till den nya listan
-        localStorage.setItem("products", JSON.stringify(initialProducts));
-        setTitle('');
-        setDescription('');
-        setPrice("");
-        setImageUrl('');
-        setStock("");
-    }
-    return (
-        <>
-            <form onSubmit={addProduct} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', background: 'lightblue', width: '20rem', margin: '0 auto', gap: '1rem' }}>
-                <input style={{ border: '0.15rem solid black' }} value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="titel" />
-                <input style={{ border: '0.15rem solid black' }} value={description} onChange={(e) => setDescription(e.target.value)} type="text" placeholder="beskrivning" />
-                <input style={{ border: '0.15rem solid black' }} value={price} onChange={(e) => setPrice(e.target.value === "" ? "" : Number(e.target.value))} type="number" placeholder="pris" />
-                <input style={{ border: '0.15rem solid black' }} value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} type="text" placeholder="url" />
-                <input style={{ border: '0.15rem solid black' }} value={stock} onChange={(e) => setStock(e.target.value === "" ? "" : Number(e.target.value))} type="number" placeholder="stock" />
-                <button style={{ border: '0.15rem solid black' }} type="submit">Submit</button>
-            </form>
-        </>
-    )
-} 1
+    if (stock === "" || price === "") return;
+
+    const newProduct: Product = {
+      id: initialProducts.length + 1,
+      title: title,
+      description: description,
+      price: price,
+      imageUrl: imageUrl,
+      stock: stock,
+    };
+    console.log(newProduct);
+    initialProducts.push(newProduct);
+    // Gör om till json sträng och sätter products till den nya listan
+    localStorage.setItem("products", JSON.stringify(initialProducts));
+    setTitle("");
+    setDescription("");
+    setPrice("");
+    setImageUrl("");
+    setStock("");
+  }
+  return (
+    <>
+      <form
+        onSubmit={addProduct}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "lightblue",
+          width: "20rem",
+          margin: "0 auto",
+          gap: "1rem",
+        }}
+      >
+        <input
+          style={{ border: "0.15rem solid black" }}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="titel"
+        />
+        <input
+          style={{ border: "0.15rem solid black" }}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          placeholder="beskrivning"
+        />
+        <input
+          style={{ border: "0.15rem solid black" }}
+          value={price}
+          onChange={(e) =>
+            setPrice(e.target.value === "" ? "" : Number(e.target.value))
+          }
+          type="number"
+          placeholder="pris"
+        />
+        <input
+          style={{ border: "0.15rem solid black" }}
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          type="text"
+          placeholder="url"
+        />
+        <input
+          style={{ border: "0.15rem solid black" }}
+          value={stock}
+          onChange={(e) =>
+            setStock(e.target.value === "" ? "" : Number(e.target.value))
+          }
+          type="number"
+          placeholder="stock"
+        />
+        <button style={{ border: "0.15rem solid black" }} type="submit">
+          Submit
+        </button>
+      </form>
+      <section>
+        {query.data?.map((product: Product) => (
+          <article key={product.id}>
+            <h2>{product.title} </h2>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+          </article>
+        ))}
+      </section>
+    </>
+  );
+}
+1;
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      v1: "http://localhost:3000",
+      "/v1": "http://localhost:3000",
     },
   },
 });
