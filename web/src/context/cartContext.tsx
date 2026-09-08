67527919db980fe2da685f67838bc0d0dfd4e444
Mattias <mattiasmeza94@hotmail.com>
2026-09-08 13:56:52 +0200

    Added cart context and add-to-cart functionality

---
+++ web/src/context/cartContext.tsx
@@ -0,0 +1,45 @@
+import type { ReactNode } from "react";
+import { createContext, useContext, useState } from "react";
+import type { CartItem, Product } from "../types";
+
+interface CartContextType {
+    cartItems: CartItem[];
+    addToCart: (product: Product) => void;
+}
+
+interface CartProviderProps {
+    children: ReactNode;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export function CartProvider({ children }: CartProviderProps) {
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    function addToCart(product: Product) {
+        console.log("Lägger till:", product);
+        setCartItems((items) => [
+            ...items,
+            {
+                product: product,
+                quantity: 1,
+            },
+        ]);
+    }
+
+    return (
+        <CartContext.Provider value={{ cartItems, addToCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export function useCart() {
+    const context = useContext(CartContext);
+
+    if (!context) {
+        throw new Error("useCart must be used inside CartProvider");
+    }
+
+    return context;
+}
