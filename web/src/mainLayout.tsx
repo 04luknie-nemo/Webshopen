9ecf85aafab2b25709936eadbc2d76c3ee10b28a
Lukas <04luknie@skola.boras.se>
2026-09-10 19:04:26 +0200

    Footer is added and wraped mainlayout with a box with minheight and put flex on main

--- web/src/mainLayout.tsx
+++ web/src/mainLayout.tsx
@@ -1,10 +1,17 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Outlet, Link as RouterLink } from "react-router";
 
 export default function MainLayout() {
   return (
-    <>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <AppBar position="static">
         <Toolbar sx={{ gap: 2 }}>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
@@ -33,7 +40,54 @@ export default function MainLayout() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Outlet />
-    </>
+      <Box component={"main"} sx={{ flex: 1 }}>
+        <Outlet />
+      </Box>
+      <Box
+        component={"footer"}
+        sx={{
+          py: 2,
+          textAlign: "center",
+          backgroundColor: "grey",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.5rem",
+        }}
+      >
+        <Typography>@2026 Webshoppen</Typography>
+        <Typography>Email: olas.priv@gmail.com</Typography>
+        <Typography>Email: mattias.priv@gmail.com</Typography>
+        <Box
+          component={"nav"}
+          sx={{ display: "flex", gap: "0.5rem", justifyContent: "center" }}
+        >
+          <Button
+            variant="contained"
+            color="secondary"
+            component={RouterLink}
+            to="/about"
+          >
+            Om Oss
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            component={RouterLink}
+            to="/contact"
+          >
+            Kontakt
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            component={RouterLink}
+            to="/policy"
+          >
+            Retur/Policy
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
