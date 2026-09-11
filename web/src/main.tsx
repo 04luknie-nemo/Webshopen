c3ac14a08ef8ebb9a414d0533dced9bf2b298255
Mattias <mattiasmeza94@hotmail.com>
2026-09-11 12:48:57 +0200

    Surrend App with Cartprovider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,13 +2,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { CartProvider } from "./context/cartContext.tsx";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={client}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
+    <StrictMode>
+        <QueryClientProvider client={client}>
+            <CartProvider>
+                <App />
+            </CartProvider>
+        </QueryClientProvider>
+    </StrictMode>,
 );
