fd349b28cd07c00b18c76adcb8d2afc8e7e98bdb
Lukas <04luknie@skola.boras.se>
2026-09-11 09:27:38 +0200

    Trying to fix as little as possible with css, admin is now looking weird but thats for later, homepage, header and footer is working as intended

--- web/src/mainLayout.tsx
+++ web/src/mainLayout.tsx
@@ -12,35 +12,56 @@ import { Outlet, Link as RouterLink } from "react-router";
 export default function MainLayout() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <AppBar position="static">
-        <Toolbar sx={{ gap: 2 }}>
+      <AppBar
+        position="static"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          height: { xs: "8rem", md: "4rem" },
+        }}
+      >
+        <Toolbar
+          sx={{
+            gap: 2,
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+          }}
+        >
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             En liten webbshop för en stor kropp 💪🏼
           </Typography>
-          <Button
-            variant="contained"
-            // Theme provider ska läggas till, kommer i annat issue
-            color="secondary"
-            component={RouterLink}
-            to="/"
-          >
-            Home
-          </Button>
-          <Button
-            variant="contained"
-            // Theme provider ska läggas till, kommer i annat issue
-            color="secondary"
-            component={RouterLink}
-            to="/admin"
-          >
-            Admin
-          </Button>
-          <IconButton component={RouterLink} to="/checkout">
-            <ShoppingCart />
-          </IconButton>
+          <Box component={"article"} sx={{ display: "flex", gap: "1rem" }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={RouterLink}
+              to="/"
+            >
+              Home
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={RouterLink}
+              to="/admin"
+            >
+              Admin
+            </Button>
+            <IconButton component={RouterLink} to="/checkout">
+              <ShoppingCart />
+            </IconButton>
+          </Box>
         </Toolbar>
       </AppBar>
-      <Box component={"main"} sx={{ flex: 1 }}>
+      <Box
+        component={"main"}
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <Outlet />
       </Box>
       <Box
@@ -53,6 +74,7 @@ export default function MainLayout() {
           display: "flex",
           flexDirection: "column",
           gap: "0.5rem",
+          marginTop: "1rem",
         }}
       >
         <Typography>@2026 Webshoppen</Typography>
