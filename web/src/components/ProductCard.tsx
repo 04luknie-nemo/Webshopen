f91a66190baefd4b32e2a4ec9d3c7385b667b375
Lukas <04luknie@skola.boras.se>
2026-09-14 09:19:55 +0200

    Added back the css changes to make sure each product card is equal size and spacing between rows

--- web/src/components/ProductCard.tsx
+++ web/src/components/ProductCard.tsx
@@ -5,62 +5,68 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Link as RouterLink } from "react-router";
-import { type Product } from "../types";
 import { useCart } from "../context/cartContext";
 import { useToast } from "../context/toastContext";
+import { type Product } from "../types";
 
 interface ProductCardProps {
   product: Product;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-    const { addToCart } = useCart();
-    const { showToast } = useToast();
+  const { addToCart } = useCart();
+  const { showToast } = useToast();
 
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt={product.title}
-                height="200"
-                image={product.imageUrl}
-            />
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "345px",
+        height: "30rem",
+      }}
+    >
+      <CardMedia
+        component="img"
+        alt={product.title}
+        height="200"
+        image={product.imageUrl}
+        sx={{ height: "15rem" }}
+      />
 
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {product.title}
-                </Typography>
+      <CardContent sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
+        <Typography gutterBottom variant="h5" component="div">
+          {product.title}
+        </Typography>
 
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                    {product.description}
-                </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {product.description}
+        </Typography>
 
-                <Typography sx={{ mt: 2, fontWeight: "bold" }}>
-                    {product.price} sek
-                </Typography>
-            </CardContent>
+        <Typography sx={{ mt: "auto", fontWeight: "bold" }}>
+          {product.price} sek
+        </Typography>
+      </CardContent>
 
-            <CardActions>
-                <Button
-                    size="small"
-                    onClick={() => {
-                        addToCart(product);
-                        showToast(
-                            `${product.title} har lagts till i kundvagnen`,
-                        );
-                    }}
-                >
-                    Buy
-                </Button>
+      <CardActions>
+        <Button
+          size="small"
+          onClick={() => {
+            addToCart(product);
+            showToast(`${product.title} har lagts till i kundvagnen`);
+          }}
+        >
+          Buy
+        </Button>
 
-                <Button
-                    component={RouterLink}
-                    to={`/product/${product.id}`}
-                    size="small"
-                >
-                    Learn More
-                </Button>
-            </CardActions>
-        </Card>
-    );
+        <Button
+          component={RouterLink}
+          to={`/product/${product.id}`}
+          size="small"
+        >
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
--- web/src/pages/homePage.tsx
+++ web/src/pages/homePage.tsx
@@ -1,40 +1,40 @@
-import Typography from "@mui/material/Typography";
 import { Alert, Container, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import { useQuery } from "@tanstack/react-query";
 import { getAllProducts } from "../api/products";
 import ProductCard from "../components/ProductCard";
 
 export default function HomePage() {
-    const getQuery = useQuery({
-        queryKey: ["products"],
-        queryFn: getAllProducts,
-    });
+  const getQuery = useQuery({
+    queryKey: ["products"],
+    queryFn: getAllProducts,
+  });
 
-    if (getQuery.isError) {
-        return (
-            <Container maxWidth="lg" sx={{ mt: 4, textAlign: "center" }}>
-                <Alert severity="error">{getQuery.error.message}</Alert>
-            </Container>
-        );
-    }
-
+  if (getQuery.isError) {
     return (
-        <Container maxWidth="lg" sx={{ mt: 4, textAlign: "center" }}>
-            <Typography
-                variant="h3"
-                component="h1"
-                gutterBottom
-                sx={{ fontSize: { xs: "2rem", md: "3rem" } }}
-            >
-                Träningsprodukter
-            </Typography>
-            <Grid container spacing={2} sx={{ justifyContent: "center" }}>
-                {getQuery.data?.map((product) => (
-                    <Grid key={product.id} size={{ xs: 12, sm: 6, md: 4 }}>
-                        <ProductCard product={product} />
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
+      <Container maxWidth="lg" sx={{ mt: 4, textAlign: "center" }}>
+        <Alert severity="error">{getQuery.error.message}</Alert>
+      </Container>
     );
+  }
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, textAlign: "center" }}>
+      <Typography
+        variant="h3"
+        component="h1"
+        gutterBottom
+        sx={{ fontSize: { xs: "2rem", md: "3rem" } }}
+      >
+        Träningsprodukter
+      </Typography>
+      <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+        {getQuery.data?.map((product) => (
+          <Grid key={product.id} size={{ xs: 12, sm: 6, md: 4 }}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
